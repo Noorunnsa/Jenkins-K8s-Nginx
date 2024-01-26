9b443af8f4fa6073fcc8bff9b3c62af5ac799a8f
Noorunnsa <108316504+Noorunnsa@users.noreply.github.com>
2024-01-26 21:05:17 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
+chmod +x minikube
+sudo mkdir -p /usr/local/bin/
+sudo install minikube /usr/local/bin/
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 minikube config set driver docker
