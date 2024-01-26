38f03756088b4cd74d2af8bdbc1d10c8ea607b08
Noorunnsa <talknasrin@gmail.com>
2024-01-26 15:28:39 +0000

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -5,27 +5,14 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo mkdir -p /etc/containerd
-sudo containerd config default > /etc/containerd/config.toml
-sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
-sudo systemctl start containerd
-sudo systemctl enable containerd
+
 
 sudo sed -i '/swap/d' /etc/fstab
 sudo swapoff -a
 
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
-sudo mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-sudo cp -r /root/.kube /home/kubernetes
-sudo chown -R kubernetes:kubernetes /home/kubernetes/.kube
-export KUBECONFIG=~/.kube/config
-
-source /home/kubernetes/.bashrc
 
 minikube config set driver docker
 
