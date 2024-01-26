6d54e20f30f4481390b7586ec18dd6dd84732962
Noorunnsa <108316504+Noorunnsa@users.noreply.github.com>
2024-01-26 21:05:56 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ sudo mkdir -p /usr/local/bin/
 sudo install minikube /usr/local/bin/
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
-minikube config set driver docker
 minikube start --driver=docker
 kubectl get po -A
 minikube kubectl -- get po -A
